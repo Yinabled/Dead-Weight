b0aeae59304a5155dee00e1c29311d6f51a759f6
Yinabled <williamyin08@gmail.com>
2017-06-06 11:08:35 -0400

    Add files via upload
    Added Rorschach Tile elements, moving tiles (kinematic character), edits to Player, etc. - rorschach collision does not work yet

--- Dead Weight/skyscrollscript.gd
+++ Dead Weight/skyscrollscript.gd
@@ -1,13 +1,21 @@
 extends TextureFrame
 
-var timePassed = 0
+var scroll_time = 0.1
+
+var timer = 0
+var temp_time = 0
 
 func _ready():
 	set_process(true)
 	
 func _process(delta):
+	timer += delta
 	var curPos = get_pos()
-	curPos.x = curPos.x - 0.1 * delta
-	if curPos.x < get_size().x * 3 / 4 * -1:
-		curPos.x = get_size().x * 1 / 4 * -1
-	set_pos(curPos)
+	
+	if (temp_time + scroll_time < timer):
+		curPos.x = curPos.x - 1
+		if curPos.x < get_size().x * 3 / 4 * -1:
+			curPos.x = get_size().x * 1 / 4 * -1
+		set_pos(curPos)
+		temp_time += scroll_time
+	
